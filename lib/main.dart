cd3ad2264f46744e485ac721a2fdf6706a8d52e0
sarath cholakkal <ssarathcholakkall@gmail.com>
2024-02-18 19:45:39 +0530

    Animated Icons

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  bool _isplay = false;
+  late AnimationController _controller;
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              if (_isplay == false) {
+                _controller.forward();
+                _isplay = true;
+              } else {
+                _controller.reverse();
+              }
+            },
+            child: AnimatedIcon(
+              icon: AnimatedIcons.play_pause,
+              progress: _controller,
+              size: 100,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
